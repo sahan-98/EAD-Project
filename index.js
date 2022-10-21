30070db8183394ad8e08a356da6addf59b32ba75
Sahan Kariyawasam <68751902+sahan-98@users.noreply.github.com>
2022-10-21 13:14:45 +0530

    Creat a DB Connection

--- index.js
+++ index.js
@@ -1,9 +1,66 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const path = require('path') 
+
+const errorHandler = require('./_helpers/error-handler');
+
+require('dotenv').config({path: __dirname + '/.env'})
+
+/* import *
+  *  routes *
+    *    here */
+// const UserRoutes = require('./routes/user-routes');
+// const EditorRoutes = require('./routes/editor-routes');
+// const ReviewerRoutes = require('./routes/reviewer-routes');
+// const AdminRoutes = require('./routes/admin-router');
+
+const MONGO_DB_PASSWORD = process.env['MONGO_DB_PASSWORD'];
+// const connectionString = `mongodb+srv://Admin:${MONGO_DB_PASSWORD}@icaf-cluster.pahle.mongodb.net/icafDB?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://EADProject:${MONGO_DB_PASSWORD}@ead.df5rgnq.mongodb.net/?retryWrites=true&w=majority`;
+
+app = express(),
+port = process.env.PORT || 4000;
+
+app.use(cors());
+
+app.use(bodyParser.json({limit: '50mb'}));
+
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+var nodemailer = require('nodemailer');
+var hbs = require('nodemailer-express-handlebars');
+app.set('views', path.join(__dirname, 'views')) 
+app.set('view engine', 'ejs') 
+
+/* global *
+  *  error *
+    *    handler */
+app.use(errorHandler);
+
+/* add *
+  *  routes *
+    *    here */
+//app.use('/', UserRoutes);
+
+// app.use('/api/v1/reviewer', ReviewerRoutes);
+// app.use('/api/v1/editor', EditorRoutes);
+// app.use('/api/v1/admin', AdminRoutes);
+// app.use('/api/v1/users/', UserRoutes);
+
+mongoose
+.connect(connectionString)
+.then(() => {
+  app.listen(port, () => {
+    console.log('Server is listening on port ' + port + `\n http://localhost:${port}`);
+});
+})
+.catch(err => {
+    console.log(err);
+});
 
 
 app.get('/home', function(req, res){
     res.send('Home Screen');
 });
-
-app.listen(3000, () => console.log('Listening on Port 3000'));
