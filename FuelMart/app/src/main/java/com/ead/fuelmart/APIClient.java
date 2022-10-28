d3681cbda4215d283cb24e19352bcf63613b9ab5
SLJayasinghe <it19126234@my.sliit.lk>
2022-10-29 01:22:58 +0530

    minor changes

--- FuelMart/app/src/main/java/com/ead/fuelmart/APIClient.java
+++ FuelMart/app/src/main/java/com/ead/fuelmart/APIClient.java
@@ -16,7 +16,7 @@ class APIClient {
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
 
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:4000")
+                .baseUrl("http://127.0.0.1:4000/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
